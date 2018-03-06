5b1b5696384e3344b91c059180438b8b1e5594b2
Aitor Marin <lolno9@gmail.com>
2018-03-06 15:49:16 +0100

    cercle constructors

---
+++ Cercle.java
@@ -0,0 +1,45 @@
+package M5UF2;
+
+public class Cercle{
+	
+	 private float x;
+	 private float y;
+	 Cercle(float xx, float yy)
+	  {
+	     x=xx;
+	     y=yy;
+	  }
+	 public Cercle() {
+	 }
+	 public Cercle(Cercle c){
+		 x=c.x;
+		 y=c.y;
+	 }
+	@Override
+	public String toString() {
+		return "Cercle [x=" + x + ", y=" + y + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(x);
+		result = prime * result + Float.floatToIntBits(y);
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cercle other = (Cercle) obj;
+		if (Float.floatToIntBits(x) != Float.floatToIntBits(other.x))
+			return false;
+		if (Float.floatToIntBits(y) != Float.floatToIntBits(other.y))
+			return false;
+		return true;
+	} 
+}
